52accb50faf4a802ef55c69d406cb17a3ed49cd3
obeyler <beyler_olivier@yahoo.fr>
2020-11-15 10:36:04 +0100

    Update disable-wifi-bluetooth.sh

--- disable-wifi-bluetooth.sh
+++ disable-wifi-bluetooth.sh
@@ -1,3 +1,6 @@
 sudo systemctl disable wpa_supplicant
 sudo systemctl disable bluetooth
 sudo systemctl disable hciuart
+#vi /boot/config.txt 
+#dtoverlay=pi3-disable-wifi
+#dtoverlay=pi3-disable-bt
